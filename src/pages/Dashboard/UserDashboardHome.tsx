3b3912a7cc81cc969108912dc7d9093a1fede34c
Favour <neutrinookoye@gmail.com>
2023-05-11 11:54:38 +0100

    done

--- src/pages/Dashboard/UserDashboardHome.tsx
+++ src/pages/Dashboard/UserDashboardHome.tsx
@@ -4,14 +4,26 @@ import { useAppDispatch, useAppSelector } from '../../app/hooks'
 import {
 	create_booking,
 	delete_booking_from_state,
+	get_user_bookings,
 } from '../../features/booking/bookingSlice'
+import {
+	apartmentReset,
+	get_apartments_by_user,
+	get_saved_apartments,
+} from '../../features/apartment/apartmentSlice'
+import { get_all_notifications } from '../../features/notification/notificationSlice'
 
 const UserDashboardHome = () => {
 	const dispatch = useAppDispatch()
 	const dataData1 = [8, 29, 7, 8, 5, 1, 2]
 	const dataData2 = [12, 19, 3, 5, 2, 3, 6]
 
-	const { bookingState } = useAppSelector((state) => state.booking)
+	const { bookingState, bookings } = useAppSelector((state) => state.booking)
+	const { user_detail } = useAppSelector((state) => state.auth)
+	const { userApartments, savedApartments } = useAppSelector(
+		(state) => state.apartment
+	)
+	const { notifications } = useAppSelector((state) => state.notification)
 
 	useEffect(() => {
 		if (bookingState) {
@@ -21,34 +33,91 @@ const UserDashboardHome = () => {
 				dispatch(delete_booking_from_state())
 			}, 100)
 		}
+
+		dispatch(get_apartments_by_user())
+		dispatch(get_all_notifications())
+		dispatch(get_saved_apartments())
+		dispatch(get_user_bookings())
+		return () => {
+			dispatch(apartmentReset())
+		}
 	}, [dispatch])
 
 	return (
 		<main className='dashboard'>
 			<h6 className=''> All Time </h6>
 			<div className='row mb-4'>
-				<div className='col-md-3 col-sm-6'>
-					<div className='stat_box stat_box1'>
-						<h5> 10 </h5>
-						<p> Apartment Booked </p>
+				{user_detail?.role !== 'USER' && (
+					<div className='col-md-3 col-sm-6'>
+						<div className='stat_box stat_box1'>
+							<h5>
+								{userApartments?.apartments
+									? userApartments?.apartments?.filter(
+											(item) => item.isAvailable === false
+									  ).length
+									: 0}
+							</h5>
+							<p> Apartments Booked </p>
+						</div>
 					</div>
-				</div>
-				<div className='col-md-3 col-sm-6'>
+				)}
+				<div
+					className={
+						user_detail?.role !== 'USER'
+							? 'col-md-3 col-sm-6'
+							: 'col-md-4 col-sm-6'
+					}
+				>
 					<div className='stat_box stat_box2'>
-						<h5> 10 </h5>
-						<p> Sheduled Booking </p>
+						{user_detail?.role === 'USER' ? (
+							<>
+								<h5>
+									{savedApartments?.apartment
+										? savedApartments?.apartment?.length
+										: 0}
+								</h5>
+								<p> My Wishlist </p>
+							</>
+						) : (
+							<>
+								<h5>
+									{userApartments?.apartments
+										? userApartments?.apartments?.length
+										: 0}
+								</h5>
+
+								<p> Listed Apartments </p>
+							</>
+						)}
 					</div>
 				</div>
-				<div className='col-md-3 col-sm-6'>
+				<div
+					className={
+						user_detail?.role !== 'USER'
+							? 'col-md-3 col-sm-6'
+							: 'col-md-4 col-sm-6'
+					}
+				>
 					<div className='stat_box stat_box3'>
-						<h5> 10 </h5>
-						<p> Check-Ins </p>
+						<h5> {bookings?.bookings ? bookings?.bookings?.length : 0} </h5>
+						<p> My Bookings </p>
 					</div>
 				</div>
-				<div className='col-md-3 col-sm-6'>
+				<div
+					className={
+						user_detail?.role !== 'USER'
+							? 'col-md-3 col-sm-6'
+							: 'col-md-4 col-sm-6'
+					}
+				>
 					<div className='stat_box stat_box4'>
-						<h5> 10 </h5>
-						<p> Check-Outs </p>
+						<h5>
+							{' '}
+							{notifications?.notifications
+								? notifications?.notifications?.length
+								: 0}{' '}
+						</h5>
+						<p> Notifications </p>
 					</div>
 				</div>
 			</div>

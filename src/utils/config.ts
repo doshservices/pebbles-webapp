36ca3a4b0cb83f879a3693162ff687659067a7f8
Favour <neutrinookoye@gmail.com>
2023-06-11 23:02:11 +0100

    done

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 const config = {
 	testUrl: 'https://pubblessignature-production.up.railway.app/api',
-	liveUrl: 'https://pubblessignature-production.up.railway.app/api',
+	liveUrl: 'https://apipebblessignature.online/',
 }
 
 export default config
